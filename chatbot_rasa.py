d891b6ecc1c195395294c9687d502c746a2aa54a
MeetPitsy <130001000+MeetPitsy@users.noreply.github.com>
2023-05-07 06:40:35 -0500

    Add files via upload

---
+++ chatbot_rasa.py
@@ -0,0 +1,12 @@
+version: "2.0"
+responses:
+  utter_greet:
+  - text: "Hello, what kind of product would you like to make?"
+
+  utter_ask_product:
+  - text: "We have various types of shirts available. What material would you like?"
+  - text: "We offer hats in different styles. What type of hat are you interested in?"
+  - text: "We have different types of bags. What kind of bag would you like?"
+
+  utter_goodbye:
+  - text: "Goodbye! Have a great day!"
